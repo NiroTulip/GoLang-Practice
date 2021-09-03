e6fe10c753955925dc84a235646be1306effd649
Oleg Parunev <paruneff@gmail.com>
2021-09-03 11:56:39 +0300

    Update 1 find dozens.go

--- BASICS/1 find dozens.go
+++ BASICS/1 find dozens.go
@@ -1,17 +1,12 @@
 /* A non-negative integer is given. Find the number of tens (that is, the second digit from the right).
-
 Input data format
 The input is a natural number not exceeding 10,000.
-
 Output data format
 Print one integer - the number of tens.  */
 
-
 /* Дано неотрицательное целое число. Найдите число десятков (то есть вторую цифру справа). 
-
 Формат входных данных
 На вход дается натуральное число, не превосходящее 10000.
-
 Формат выходных данных
 Выведите одно целое число - число десятков. */
 
